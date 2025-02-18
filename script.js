1f01f3043c46899a72ffe12c8cb8ec5638654560
semawon okedairo <okedairosemawon@gmail.com>
2025-02-18 16:36:35 +0100

    worked on designing the website

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// const addButton  = document.getElementById("addBtn");
+// const taskList = document.getElementById("taskList");
+// const taskInput = document.getElementById("inputTask");
+
+// function addBtn() {
+//     // trim the value inside the plaaceholder
+//     const task = taskInput.Value.trim();
+
+//     if (task) {
+//         createTaskElement(task);
+//         taskInput.value = " ";
+//     }else {
+//         alert("Please enter a task!")
+//     }
+
+// }
+// addButton.addEventListener('click', addBtn);
+
+// // adds the LI element to the taskList 
+// function createTaskElement(task) {
+//     const listItem = dosument.createElement('li');
+//     listItem.textContent = task;
+//     taskList.appendChild(listItem);
+// }
